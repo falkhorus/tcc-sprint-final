6f926d92a2171a67a81d183b8798bb152dae5d43
Victor Falcão <falkhorus@gmail.com>
2025-11-20 20:18:28 -0300

    first commit - tela de login - tela de cadastro

---
+++ src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+
+export const routes: Routes = [
+
+    // se o caminho for vazio na url, redireciona para login
+    {path: '', component: HomeComponent, pathMatch: 'full'},
+
+
+    //Essas são as rotas principais
+    { path: 'login', component: LoginComponent} ,
+    {path: 'register', component: RegisterComponent}   
+
+
+
+];
